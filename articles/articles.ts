2905332f4055a321b6e554b884e6e86ee01db5f9
Fay Carsons <faycarsons23@gmail.com>
2024-01-04 14:26:44 -0500

    fixing issue in blog post indexing

--- articles/articles.ts
+++ articles/articles.ts
@@ -2,18 +2,23 @@ import { posts, Post } from "./posts.ts";
 import { Result, log } from "../lib/utilities.ts";
 import { init_shaders } from "../sketches/clouds/clouds.ts";
 
+const AUTHOR_NAME = "Fay Carsons";
+
 let post_index = 0;
 let post_cache: Post[] = [];
 
 const p_class = "font-mono text-sm my-4 mx-auto";
 const info_class = "font-mono text-gray-500 text-xs mb-2";
 const h1_class = "text-3xl font-bold mb-2";
+const err_div_class = "flex flex-col items-center justify-center";
+const err_h2_class = "text-2xl font-bold"
 
 const classes: { [key: string]: string } = {
   p: p_class,
   h1: h1_class,
 };
 
+// Helper for fetching & validating DOM elements - returns 'Result class'
 const get_id = (id: string): Result<HTMLElement, Error> => {
   let maybe = document.getElementById(id);
 
@@ -22,18 +27,21 @@ const get_id = (id: string): Result<HTMLElement, Error> => {
     : Result.err(new Error(`Cannot access ${id} element`));
 };
 
+// Sets date and author of post
 const set_post_info = (container: HTMLDivElement, date_string: string) => {
   const date = document.createElement("p");
   date.className = info_class;
   date.innerText = date_string;
   const name = document.createElement("p");
   name.className = info_class;
-  name.innerText = "Fay Carsons";
+  name.innerText = AUTHOR_NAME;
 
   container.appendChild(date);
   container.appendChild(name);
 };
 
+// Gets blog root, creates element from Post object, appends to root, 
+// and caches neighboring posts 
 const set_post = (post: Post): Result<null, Error> => {
   const maybe_header = get_id("header");
   if (!maybe_header.ok || !(maybe_header.inner instanceof HTMLElement)) {
@@ -64,13 +72,22 @@ const set_post = (post: Post): Result<null, Error> => {
   return Result.ok(null);
 };
 
-const enum Direction {
-  Left = -1,
-  Right = 1,
+const set_error_message = () => {
+  const maybe_post = get_id('post');
+  const post = maybe_post.unwrap<HTMLDivElement>();
+  const div = document.createElement('div');
+  div.className = err_div_class;
+  const err_msg = document.createElement('h2')
+  err_msg.innerText = 'Oops we\'re having some problems :0'
+  err_msg.className = err_h2_class
+  div.appendChild(err_msg)
+  post.appendChild(div)
 }
 
-const fetch_post = async (i: number): Promise<Post> => {
-  const url = posts[i];
+// Fetches post from URL in posts[i]
+const fetch_post = async (idx: number): Promise<Post> => {
+  console.log('fetching !')
+  const url = posts[idx];
   const req = await fetch(url);
   if (!req.ok) {
     throw new Error(req.statusText);
@@ -83,6 +100,8 @@ const fetch_post = async (i: number): Promise<Post> => {
   return json;
 };
 
+// Caches the posts to the left and right of the current post 
+// this is to decrease loading time and (hopefully) improve UX
 const cache_posts = async () => {
   for (
     let i = Math.abs(post_index - 1) % posts.length;
@@ -97,9 +116,17 @@ const cache_posts = async () => {
   console.log(post_cache);
 };
 
+// Creates onClick fn for nav buttons
+const enum Direction {
+  Left = -1,
+  Right = 1,
+}
 const create_post_nav = (dir: Direction) => {
   return async (_: UIEvent) => {
-    post_index = log(Math.abs(post_index + dir.valueOf()) % posts.length);
+    post_index = log((post_index + dir.valueOf()) % posts.length);
+    if (post_index < 0) {
+      post_index = posts.length - 1
+    }
     try {
       const post = post_cache[post_index]
         ? post_cache[post_index]
@@ -111,7 +138,7 @@ const create_post_nav = (dir: Direction) => {
   };
 };
 
-async function init() {
+async function init(): Promise<Result<null, Error>> {
   const buttons: Result<HTMLElement, Error>[] = [
     get_id("left"),
     get_id("right"),
@@ -129,10 +156,13 @@ async function init() {
     const post = await fetch_post(post_index);
     set_post(post);
   } catch (e) {
-    console.error(e);
+    return Result.err(e) as Result<null, Error>
   }
 
   init_shaders();
+  return Result.ok(null)
 }
 
-init();
+ if (! (await init()).ok) {
+    set_error_message()
+ }
