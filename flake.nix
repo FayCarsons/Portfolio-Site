8ff8ceb1d1ce19165dac71c2065d765b9d2e2f70
Fay Carsons <faycarsons@Mac.fios-router.home>
2025-06-16 16:05:13 -0400

    Nix pls

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Portfolio site with blog parser";
-
+  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -8,33 +8,32 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+      let pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "portfolio-site";
           src = ./.;
           
-          buildInputs = with pkgs; [
-            ghc
-            cabal-install
-            nodejs
-            npm
+          buildInputs = with pkgs; [ 
+            ghc 
+            cabal-install 
+            nodejs 
+            npm 
           ];
           
           buildPhase = ''
-            # Build blog parser
+            echo "Building blog parser..."
             cd blog-parser
             cabal update
+            cabal configure
             cabal build
-            cabal run blog-parser
+            cabal run
             cd ..
             
-            # Build frontend
+            echo "Building frontend..."
             cd frontend
             npm ci
             npm run build
-            cd ..
           '';
           
           installPhase = ''
@@ -42,13 +41,9 @@
           '';
         };
         
+        # Development shell (optional, for local development)
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            ghc
-            cabal-install
-            nodejs
-            npm
-          ];
+          buildInputs = with pkgs; [ ghc cabal-install nodejs npm ];
         };
       });
 }
