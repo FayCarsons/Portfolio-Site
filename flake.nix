4df4b0a74cd5996880494d991e2329fd4b54db59
Fay Carsons <faycarsons@Mac.fios-router.home>
2025-06-16 16:50:33 -0400

    Fixed flake??

--- flake.nix
+++ flake.nix
@@ -18,20 +18,36 @@
             ghc 
             cabal-install 
             nodejs
-            nodePackages.npm  # Explicitly add npm
+            nodePackages.npm
+            # System dependencies for Haskell packages
+            zlib
+            zlib.dev
+            pkg-config
           ];
           
+          # Make sure pkg-config can find the libraries
+          PKG_CONFIG_PATH = "${pkgs.zlib.dev}/lib/pkgconfig";
+          
           buildPhase = ''
+            # Set up temporary home directory for cabal
+            export HOME=$TMPDIR
+            mkdir -p $HOME/.cabal
+            
             echo "Building blog parser..."
             cd blog-parser
+            
+            # Initialize cabal in the temporary home
+            cabal user-config init
             cabal update
             cabal configure
             cabal build
-            cabal run
             cd ..
             
             echo "Building frontend..."
             cd frontend
+            
+            # Set npm cache to temporary directory
+            export npm_config_cache=$TMPDIR/.npm
             npm ci
             npm run build
           '';
@@ -46,7 +62,10 @@
             ghc 
             cabal-install 
             nodejs 
-            nodePackages.npm 
+            nodePackages.npm
+            zlib
+            zlib.dev
+            pkg-config
           ];
         };
       });
